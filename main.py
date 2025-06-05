73487ff24c54d85134dad6ab844975203ed5bfca
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-06-05 12:41:13 +0100

    create seller_reviews script

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Controller:
 
 if __name__ == "__main__":
     controller = Controller("./db/parana.db")
-    res = controller.db.fetch("SELECT * FROM sellers;")
 
+    res = controller.db.populate("./db/scripts/seller_reviews.txt")
+    res = controller.db.fetch("SELECT * FROM seller_reviews;")
     print(res)
