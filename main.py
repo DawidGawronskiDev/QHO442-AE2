4313a24f50bbfea6962adae21428e12f5da3b4d6
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-06-05 12:49:25 +0100

    Create product_reviews.txt

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ class Controller:
 if __name__ == "__main__":
     controller = Controller("./db/parana.db")
 
-    res = controller.db.populate("./db/scripts/seller_reviews.txt")
-    res = controller.db.fetch("SELECT * FROM seller_reviews;")
+    controller.db.populate("./db/scripts/product_reviews.txt")
+    res = controller.db.fetch("SELECT * FROM product_reviews;")
     print(res)
