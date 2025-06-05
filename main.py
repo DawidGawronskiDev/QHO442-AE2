2ee6c821222ade24aacf06b4f5ce94f61a882fa3
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-06-05 11:45:17 +0100

    populate db

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    print("Hello, World!")
+from db import Database
+
+class Main:
+    def __init__(self, db_path):
+        self.db = Database(db_path)
 
 if __name__ == "__main__":
-    main()
+    main = Main("./db/parana.db")
+
+    res = main.db.fetch("SELECT * FROM sellers;")
+    print(res)
