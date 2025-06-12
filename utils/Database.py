24e502ffea064064f58689ecde54edf3d52393f0
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-06-12 15:22:37 +0100

    comment database class

--- utils/Database.py
+++ utils/Database.py
@@ -1,16 +1,24 @@
 import sqlite3
 
 class Database:
+    """A class to handle database operations using SQLite."""
     def __init__(self, db_path):
+        """
+        Initializes the Database instance with a connection to the SQLite database.
+        """
         self.con = sqlite3.connect(db_path)
 
     def exe(self, query, args=()):
+        """
+        Executes a SQL query with the provided arguments.
+        """
         with self.con:
             cur = self.con.cursor()
             cur.execute(query, args)
             cur.close()
 
     def fetch_one(self, query, args=()):
+        """Fetches a single row from the database based on the provided query and arguments."""
         cur = self.con.cursor()
         cur.execute(query, args)
         row = cur.fetchone()
@@ -18,6 +26,7 @@ class Database:
         return row
 
     def fetch_many(self, query, args=()):
+        """Fetches multiple rows from the database based on the provided query and arguments."""
         cur = self.con.cursor()
         cur.execute(query, args)
         rows = cur.fetchall()
@@ -25,15 +34,19 @@ class Database:
         return rows
 
     def commit(self):
+        """Commits the current transaction to the database."""
         self.con.commit()
 
     def rollback(self):
+        """Rolls back the current transaction in case of an error."""
         self.con.rollback()
 
     def begin_transaction(self):
+        """Begins a new transaction."""
         self.con.execute("BEGIN TRANSACTION;")
 
     def populate(self, script_path):
+        """Executes a SQL script file to populate the database."""
         with open(script_path, 'r') as file:
             script = file.read()
             statements = script.split(";")
